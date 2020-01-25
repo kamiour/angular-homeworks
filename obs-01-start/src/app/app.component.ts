600484490fd2a36f4d2ec2a12eb721c8847a9870
Mykhailo Kazakov <mykhailo.kazakov@requestum.com>
2020-01-25 22:28:47 +0200

    completed observables section

---
+++ obs-01-start/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated: boolean = false;
+  activatedSub: Subscription;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe((data) => {
+      this.userActivated = data;
+    })
+  }
+  
+  ngOnDestroy() {
+    this.activatedSub.unsubscribe();
+  }
+}
