8f3dca60039dcc942e01e427998eb73169308b7e
Mykhailo Kazakov <mykhailo.kazakov@requestum.com>
2020-01-28 01:55:00 +0200

    started http request module

---
+++ http-01-start/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post.model';
+import { PostsService } from './posts.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+
+  constructor(private http: HttpClient, private postsService: PostsService) {}
+
+  ngOnInit() {
+    this.isFetching = true;
+    this.postsService.fetchPosts()
+      .subscribe((posts) => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      })
+  }
+
+  onCreatePost(postData: Post) {
+    this.postsService.createAndStorePost(postData.title, postData.content);
+  }
+
+  onFetchPosts() {
+    this.isFetching = true;
+    this.postsService.fetchPosts()
+      .subscribe((posts) => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      })
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+}
